bb220078a83180bd8222e8da3f193f83207d9b92
bvlqz <39110680+bvlqz@users.noreply.github.com>
2025-05-04 23:10:23 +0000

    Deploying to gh-pages from @ bvlqz/playground@6045d2576233c558e4e111e3ab8d141d19042d65 🚀

---
+++ _deps/imgui_external-subbuild/CMakeFiles/imgui_external-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imgui_external-populate"
+  "CMakeFiles/imgui_external-populate-complete"
+  "imgui_external-populate-prefix/src/imgui_external-populate-stamp/imgui_external-populate-build"
+  "imgui_external-populate-prefix/src/imgui_external-populate-stamp/imgui_external-populate-configure"
+  "imgui_external-populate-prefix/src/imgui_external-populate-stamp/imgui_external-populate-download"
+  "imgui_external-populate-prefix/src/imgui_external-populate-stamp/imgui_external-populate-install"
+  "imgui_external-populate-prefix/src/imgui_external-populate-stamp/imgui_external-populate-mkdir"
+  "imgui_external-populate-prefix/src/imgui_external-populate-stamp/imgui_external-populate-patch"
+  "imgui_external-populate-prefix/src/imgui_external-populate-stamp/imgui_external-populate-test"
+  "imgui_external-populate-prefix/src/imgui_external-populate-stamp/imgui_external-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/imgui_external-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _deps/imgui_external-subbuild/CMakeFiles/imgui_external-populate.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for imgui_external-populate.
+# This may be replaced when dependencies are built.
